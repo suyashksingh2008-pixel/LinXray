b7fcc8f9419efa3f6ec9f7f8d464b50d1a767e01
suyash kumar singh <suyashksingh2008@gmail.com>
2026-08-29 22:37:59 +0530

    fixing report generator.py

--- report generator.py
+++ report generator.py
@@ -1,5 +1,6 @@
 from config import Database_file
+import sqlite3
 def get_scan_history():
-    conn = sqlite3.connect(DATABASE_FILE)
+    conn = sqlite3.connect(Database_file)
     cursor = conn.cursor()
     return cursor.execute("SELECT * FROM SCANS").fetchall()
