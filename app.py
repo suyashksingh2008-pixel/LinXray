619e62b8beef73a439eca9cc8cc49273f176477d
gaureshbhatia <gaureshbhatia@gmail.com>
2026-08-29 12:48:46 +0530

    Logo Implemented in Title

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ import streamlit_authenticator as stauth
 from streamlit_lottie import st_lottie
 from PIL import Image
 import sqlite3
+
+#Page Title
+st.set_page_config(page_title="LinXray", page_icon="assets/logo.png")
